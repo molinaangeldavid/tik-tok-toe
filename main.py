2f5faf05fc5d2bc8221b37ddab25cbf4c05ad48f
David molina <molina.angeldavid@gmail.com>
2022-06-26 22:10:35 -0300

    fix the print table

--- main.py
+++ main.py
@@ -1,53 +1,91 @@
 import os
 from random import choice,randint
+
+from sqlalchemy import false
 # SYMBOLS = ['X','O']
 
 def validarOpcion():
-
-    opcion = input("Opcion: ")
-    while not(opcion.isnumeric() and (int(opcion) > 0 or int(opcion) < 4)):
-        opcion = input("ERROR!! Ingrese una opcion valida")
-    opcion = int(opcion)
-    return opcion
+    option = input("Option: ")
+    # if (menu == "menu"):
+    while not(option.isnumeric() and (int(option) > 0 and int(option) < 4)):
+        option = input("ERROR!! Input a valid option")
+    # else:
+        # if (menu == "game"):
+        #     while not(option.isnumeric() and (int(option) > 0 and int(option) < 3)):
+        #         option = input("ERROR!! Input a valid option")
+    option = int(option)
+    return option
 
 def printTable(space):
     print(f'''
             {space[0][0]} |  {space[0][1]}  | {space[0][2]}
-        --------------------
+        ------------------
             {space[1][0]} |  {space[1][1]}  | {space[1][2]}
-        --------------------
-            {space[2][0]} |  {space[2][0]}  | {space[2][0]}
+        ------------------
+            {space[2][0]} |  {space[2][1]}  | {space[2][2]}
         ''')
 
-def validarGame(table):
+def playComputer():
+    random_choice = randint(0,2)
+    fila = random_choice
+    random_choice = randint(0,2)
+    columna = random_choice
+    return fila,columna
+
+def validarGame(table,signo,player):
     for x in table:
-        for col in x:
-            pass
+        if (x == [signo,signo,signo]):
+            print(f"Winner: {player}")
+            return False
+    new_table = []
+    for x in range(3):
+        for col in range(3):
+            new_table[x][col] = table[col][x]
+            if (x == col and table[x][col] =="X" or table[x][col] == 'O'):
+                return False
+            else:
+                tablereverse = table[x][::-1]
+                if (x == col and tablereverse[x][col]=="X" or tablereverse[x][col]=="O"):
+                    return False
+            
+    for x in new_table:
+        if (x == [signo,signo,signo]):
+            print(f"Winner: {player}")
+            return False
+    
 
 def playGame():
-    space = [[" "," "," "],
-            [" "," "," "],
-            [" "," "," "]]
+    space = [["","",""],
+            ["","",""],
+            ["","",""]]
     estado = True
+    player = input("Write your name: ")
     while(estado):
+        print(player)
         printTable(space)
         print("Fila")
         fila = validarOpcion()
         print("Columna")
         columna = validarOpcion()
-        while not (space[fila-1][columna-1] == " "):    
+        while not (space[fila-1][columna-1] == "" or space[fila-1][columna-1] == "O"):
             print("ERROR")
             fila = validarOpcion()
             print("Columna")
             columna = validarOpcion()
         space[fila-1][columna-1] = "X"
+        print(space[fila-1][columna-1])
+        # validarGame(space,"X",player)
+        printTable(space)
+        print(playComputer())
+        fila = playComputer()[0]
+        columna = playComputer()[1]
+        while not (space[fila-1][columna-1] == "" or space[fila-1][columna-1] == "X"):
+            fila = playComputer()[0]
+            columna = playComputer()[1]
+        space[fila-1][columna-1] = "O"
+        print(space[fila-1][columna-1])
+        # validarGame(table,'O')
         
-        random_choice = randint(0,8)
-        fila = random_choice
-        columna = random_choice
-        while not (space[fila-1][columna-1] == " "):
-            fila = random_choice
-            columna = random_choice
 
 def play():
     print('''
